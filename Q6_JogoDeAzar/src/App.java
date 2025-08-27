e1c1e0b00e8a3999418f5076b4820b05625f680d
Arthur Ávila <102988548+ItsAvila@users.noreply.github.com>
2025-08-27 18:23:04 +0000

    Realização Q6

--- Q6_JogoDeAzar/src/App.java
+++ Q6_JogoDeAzar/src/App.java
@@ -1,10 +1,17 @@
 public class App {
     public static void main(String[] args) {
-        Dado d6 = new Dado(6);
-        Dado d20 = new Dado(20);
+        IDado d6 = new DadoHistorico(new Dado(6));
+        IDado d20 = new DadoHistorico(new Dado(20));
+
+        d6.rolar();
+        d6.rolar();
         d6.rolar();
         d20.rolar();
-        System.out.println("D6: " + d6.getValor());
-        System.out.println("D20: " + d20.getValor());
+
+        System.out.println("Último valor D6: " + d6.getValor());
+        System.out.println("Histórico D6: " + ((DadoHistorico) d6).getHistorico());
+
+        System.out.println("Último valor D20: " + d20.getValor());
+        System.out.println("Histórico D20: " + ((DadoHistorico) d20).getHistorico());
     }
 }
---
+++ Q6_JogoDeAzar/src/DadoDecorator.java
@@ -0,0 +1,17 @@
+public abstract class DadoDecorator implements IDado {
+    protected IDado dado;
+
+    public DadoDecorator(IDado dado) {
+        this.dado = dado;
+    }
+
+    @Override
+    public void rolar() {
+        dado.rolar();
+    }
+
+    @Override
+    public int getValor() {
+        return dado.getValor();
+    }
+}

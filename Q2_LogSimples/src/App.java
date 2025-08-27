303797e654285c7b0b8f5c9d0aaa8f674d453b3d
LucasGL1 <129865646+LucasGL1@users.noreply.github.com>
2025-08-27 17:44:52 +0000

    Realização Q2

--- Q2_LogSimples/src/App.java
+++ Q2_LogSimples/src/App.java
@@ -1,11 +1,23 @@
+//OK
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        LogSimples log = new LogSimples();
+        LogSimples log = LogSimples.getInstancia();
+
         log.log("Mensagem de alerta 1");
         log.log("Mensagem de alarme 23");
         log.log("Mensagem de PANICO!!");
 
-        for(String m:log){
+        for (String m : log) {
+            System.out.println(m);
+        }
+
+        LogSimples outroLog = LogSimples.getInstancia();
+        outroLog.log("Mensagem adicionada de outro ponto do programa");
+
+        System.out.println("\nLog completo:");
+        for (String m : log) { 
             System.out.println(m);
         }
     }

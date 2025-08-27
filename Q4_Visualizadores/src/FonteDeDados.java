d4323936ffd77e9e710329b3a5d8bd31a25ab04a
Arthur Ávila <102988548+ItsAvila@users.noreply.github.com>
2025-08-27 18:01:54 +0000

    Realização Q4

--- Q4_Visualizadores/src/FonteDeDados.java
+++ Q4_Visualizadores/src/FonteDeDados.java
@@ -2,23 +2,40 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class FonteDeDados{
+public class FonteDeDados {
     private List<Integer> lst;
+    private List<Observer> observers;
 
-    public FonteDeDados(){
+    public FonteDeDados() {
         lst = new LinkedList<>();
+        observers = new ArrayList<>();
     }
 
-    public void add(Integer value){
-        if (value < 0) throw new IllegalArgumentException("Valor invalido");
+    public void addObserver(Observer o) {
+        observers.add(o);
+    }
+
+    public void removeObserver(Observer o) {
+        observers.remove(o);
+    }
+
+    public void add(Integer value) {
+        if (value < 0) throw new IllegalArgumentException("Valor inválido");
         lst.add(value);
+        notificarObservadores();
     }
 
-    public int quantidade(){
+    public int quantidade() {
         return lst.size();
     }
 
-    public List<Integer> getValores(){
+    public List<Integer> getValores() {
         return new ArrayList<>(lst);
+    }
+
+    private void notificarObservadores() {
+        for (Observer o : observers) {
+            o.atualizar(getValores());
+        }
     }
 }
